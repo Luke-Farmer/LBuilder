9ddc61a864d01947957838dffe11650498b04fed
Luke Farmer <luke@lukefarmer.co.uk>
2023-08-09 02:33:36 +0100

    feat: published routes, styled authentication:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,17 +12,17 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form class="auth-form" method="POST" action="{{ route('login') }}" autocomplete="off">
             @csrf
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" role="input" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="off" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" role="input" class="block mt-1 w-full" type="password" name="password" required autocomplete="off" />
             </div>
 
             <div class="block mt-4">
@@ -34,13 +34,13 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
                 <x-button class="ml-4">
-                    {{ __('Log in') }}
+                    {{ __('Login') }}
                 </x-button>
             </div>
         </form>
