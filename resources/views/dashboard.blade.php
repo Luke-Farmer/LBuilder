3fae3ca79f912159f41ff31031693aafac305fc5
Luke Farmer <luke@lukefarmer.co.uk>
2023-08-12 21:31:55 +0100

    feat: Create add, edit, delete, restore page functionality and styles:

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
-   <div class="grid grid-cols-12 gap-0">
+    @livewire('navigation-menu')
+   <div class="grid grid-cols-12 gap-0 bg-[#11212D]">
        <div class="col-span-2">
            <x-sidebar />
        </div>
-       <div class="col-span-10 bg-[#0C0D13]">
-           @livewire('navigation-menu')
+       <div class="col-span-10 bg-[#0C0D13] rounded-tl-md">
            <div class="p-8">
                <div class="grid grid-cols-3 gap-8">
                    <div class="col-span-1 p-8 bg-[#14151F] rounded-md">
                        <p class="text-white text-2xl font-bold"><i class="bi bi-cup-hot-fill text-[#1AB188]"></i> Welcome Back, {{ Auth::user()->name }}!</p>
                        <p class="text-white text-md mt-4">Full Name: {{ Auth::user()->name }}</p>
                        <p class="text-white text-md">Email Address: {{ Auth::user()->email }}</p>
-                       <a href="" class="mt-4 inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit Profile</a>
+                       <a href="" class="rounded-md mt-4 inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit Profile</a>
                    </div>
                    <div class="col-span-2 p-8 bg-[#14151F] rounded-md">
                        <p class="text-white text-2xl font-bold">Statistics Card</p>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,25 +1,25 @@
 <x-app-layout>
-    <div class="grid grid-cols-12 gap-0">
+    @livewire('navigation-menu')
+    <div class="grid grid-cols-12 gap-0 bg-[#11212D]">
         <div class="col-span-2">
             <x-sidebar />
         </div>
-        <div class="col-span-10 bg-[#0C0D13]">
-            @livewire('navigation-menu')
+        <div class="col-span-10 bg-[#0C0D13] rounded-tl-md">
             <div class="p-8">
                 <div class="grid grid-cols-1 gap-8 mb-8">
                     <div class="col-span-1 p-8 bg-[#14151F] rounded-md">
-                        <form class="p-3 ps-0 mb-0" action="{{ route('pages.store') }}" method="POST">
+                        <form class="" action="{{ route('pages.store') }}" method="POST">
                             @method('POST')
                             @csrf
                             <p class="text-white text-2xl font-bold mb-4">Create a New Page</p>
                             <div class="">
                                 <div class="flex flex-col mb-4">
                                     <label class="text-white text-md">Title - <span class="text-xs">(Page Identifier)</span></label>
-                                    <input class="rounded-md" type="text" label="title" name="title" value="">
+                                    <input class="rounded-md text-white bg-[#282a36] border-0" type="text" label="title" name="title" value="">
                                 </div>
                                 <div class="flex flex-col">
-                                    <label class="text-white text-md">URL - <span class="text-xs">(Don't include "/" at the start or end)</span></label>
-                                    <input class="rounded-md" type="text" label="slug" name="slug" value="">
+                                    <label class="text-white text-md">URL - <span class="text-xs">(Don't include "/" at the start or end - use a single slash to create the homepage)</span></label>
+                                    <input class="rounded-md text-white bg-[#282a36] border-0" type="text" label="slug" name="slug" value="">
                                 </div>
                             </div>
                             <input href="{{ route('pages.store') }}" value="Create" type="submit" class="rounded-md hover:cursor-pointer mt-4 inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
@@ -32,51 +32,58 @@
                             <div>
                                 <p class="text-white text-2xl font-bold mb-4">All Existing Pages</p>
                             </div>
-                            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                            <div class="overflow-x-auto">
                                 <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
                                     <table class="min-w-full leading-normal">
                                         <thead>
                                         <tr>
-                                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Page Title</th>
-                                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Url</th>
-                                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Last Modified</th>
-                                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Draft/Published</th>
-                                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-[#1AB188]"></th>
+                                            <th class="px-5 py-3 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Page Title</th>
+                                            <th class="px-5 py-3 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Url</th>
+                                            <th class="px-5 py-3 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Last Modified</th>
+                                            <th class="px-5 py-3 bg-[#1AB188] text-left text-xs font-semibold text-white uppercase tracking-wider">Draft/Published</th>
+                                            <th class="px-5 py-3 bg-[#1AB188]"></th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                             @foreach($pages as $page)
-                                            <tr>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                    <div class="flex">
-                                                        <div class="ml-3">
-                                                            <p class="text-gray-900 whitespace-no-wrap">{{ $page->title }}</p>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                    <p class="text-gray-900 whitespace-no-wrap">{{ $page->slug }}</p>
-                                                </td>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                    <p class="text-gray-900 whitespace-no-wrap">{{ $page->updated_at }}</p>
-                                                </td>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                    @if($page->is_draft == "1")
-                                                        <span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
-                                                          <span aria-hidden class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
-                                                          <span class="relative">Draft</span>
-                                                        </span>
-                                                    @elseif($page->is_draft == "0")
-                                                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                                          <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                          <span class="relative">Published</span>
-                                                        </span>
-                                                    @endif
-                                                </td>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                                                    <a href="{{ route('pages.edit', $page->id) }}" class="rounded-md hover:cursor-pointer mt-4 inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit</a>
-                                                </td>
-                                            </tr>
+                                                @if($page->is_deleted !== "1")
+                                                    <tr>
+                                                        <td class="px-5 py-5 bg-[#282a36] border-0 text-sm">
+                                                            <div class="flex">
+                                                                <div class="">
+                                                                    <p class="text-white whitespace-no-wrap">{{ $page->title }}</p>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                        <td class="px-5 py-5 bg-[#282a36] border-0 text-sm">
+                                                            <p class="text-white whitespace-no-wrap">{{ $page->slug }}</p>
+                                                        </td>
+                                                        <td class="px-5 py-5 bg-[#282a36] border-0 text-sm">
+                                                            <p class="text-white whitespace-no-wrap">{{ $page->updated_at }}</p>
+                                                        </td>
+                                                        <td class="px-5 py-5 bg-[#282a36] border-0 text-sm">
+                                                            @if($page->is_draft == "1")
+                                                                <span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+                                                                  <span aria-hidden class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
+                                                                  <span class="relative text-white">Draft</span>
+                                                                </span>
+                                                            @elseif($page->is_draft == "0")
+                                                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                                  <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                                  <span class="relative text-white">Published</span>
+                                                                </span>
+                                                            @endif
+                                                        </td>
+                                                        <td class="px-5 py-5 bg-[#282a36] border-0 text-sm text-right flex justify-end items-center">
+                                                            <a href="{{ route('pages.edit', $page->id) }}" class="rounded-md hover:cursor-pointer inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit</a>
+                                                            <form class="ml-4" action="{{ route('pages.destroy', $page->id) }}" method="POST">
+                                                                @method('DELETE')
+                                                                @csrf
+                                                                <input class="rounded-md hover:cursor-pointer inline-flex items-center px-4 py-2 border border-transparent font-semibold text-xs text-white uppercase tracking-widest bg-[#1AB188] hover:bg-[#179b77] focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" value="Delete" type="submit" onclick="clicked()">
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+                                                @endif
                                             @endforeach
                                         </tbody>
                                     </table>
