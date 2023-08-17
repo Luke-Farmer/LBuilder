d067405b8ac8b43d0b1903fcf1fa5c36365fe009
Luke Farmer <luke@lukefarmer.co.uk>
2023-08-17 06:27:48 +0200

    feat: create media uploads and gallery (#34)

---
+++ app/Http/Controllers/MediaController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Component;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('media.index')
+            ->withMedias($medias = Media::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) : RedirectResponse
+    {
+        $modelUser = User::find($request->user()->id);
+        $modelUser->addMediaFromRequest('file')->toMediaCollection();
+        $modelUser->save();
+
+        return redirect()
+            ->back()
+            ->with('message', 'File Uploaded.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Component $component)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
