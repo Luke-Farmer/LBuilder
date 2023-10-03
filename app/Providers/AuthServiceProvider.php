877bd2dd5119663e8dcb034284815cae099e1cba
Luke Farmer <luke@lukefarmer.co.uk>
2023-10-03 23:27:44 +0100

    bug: gate tests

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,46 +23,46 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        // dashboard
-//        Gate::define('view_dashboard', function(User $user) {
-//            return $user->hasPermissionTo('view dashboard');
-//        });
-//
-//        // pages
-//        Gate::define('create_edit_pages', function(User $user) {
-//            return $user->hasPermissionTo('create and edit pages');
-//        });
-//
-//        Gate::define('delete_pages', function(User $user) {
-//            return $user->hasPermissionTo('delete pages');
-//        });
-//
-//        Gate::define('wipe_pages', function(User $user) {
-//            return $user->hasPermissionTo('permanently delete pages');
-//        });
-//
-//        // components
-//        Gate::define('create_edit_components', function(User $user) {
-//            return $user->hasPermissionTo('create and edit components');
-//        });
-//
-//        Gate::define('delete_components', function(User $user) {
-//            return $user->hasPermissionTo('delete components');
-//        });
-//
-//        Gate::define('wipe_components', function(User $user) {
-//            return $user->hasPermissionTo('permanently delete components');
-//        });
-//
-//        // media
-//        Gate::define('add_media', function(User $user) {
-//            return $user->hasPermissionTo('add media');
-//        });
-//
-//        // settings
-//        Gate::define('manage_settings', function(User $user) {
-//            return $user->hasPermissionTo('manage users');
-//        });
+        // dashboard
+        Gate::define('view_dashboard', function(User $user) {
+            return $user->hasPermissionTo('view dashboard');
+        });
+
+        // pages
+        Gate::define('create_edit_pages', function(User $user) {
+            return $user->hasPermissionTo('create and edit pages');
+        });
+
+        Gate::define('delete_pages', function(User $user) {
+            return $user->hasPermissionTo('delete pages');
+        });
+
+        Gate::define('wipe_pages', function(User $user) {
+            return $user->hasPermissionTo('permanently delete pages');
+        });
+
+        // components
+        Gate::define('create_edit_components', function(User $user) {
+            return $user->hasPermissionTo('create and edit components');
+        });
+
+        Gate::define('delete_components', function(User $user) {
+            return $user->hasPermissionTo('delete components');
+        });
+
+        Gate::define('wipe_components', function(User $user) {
+            return $user->hasPermissionTo('permanently delete components');
+        });
+
+        // media
+        Gate::define('add_media', function(User $user) {
+            return $user->hasPermissionTo('add media');
+        });
+
+        // settings
+        Gate::define('manage_settings', function(User $user) {
+            return $user->hasPermissionTo('manage users');
+        });
     }
 
 }
